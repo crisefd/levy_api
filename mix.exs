5ea4cf71cb08906876443f4b21cbe77451fb2471
Chris Fuertes <cristhian.fuertes@correounivalle.edu.co>
2019-05-09 22:33:25 -0500

    Adding test_seeds.exs

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule LevyApi.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.reset", "ecto.migrate", "run test/test_seeds.exs", "test"]
     ]
   end
 end
---
+++ test/test_seeds.exs
@@ -0,0 +1,21 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     LevyApi.Repo.insert!(%LevyApi.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+alias LevyApi.Accounts.User
+alias LevyApi.Repo
+
+# Dummy John Doe user
+
+Repo.insert %User{id: 1,
+                  username: "john.doe@dummy.com",
+                  name: "John",
+                  surname: "Doe"}
